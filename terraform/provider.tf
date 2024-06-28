62a90ce9f2b45a6430faa070be69399ab12d0e50
mrkhalnayak <141492925+mrkhalnayak@users.noreply.github.com>
2024-06-28 17:49:42 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,16 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
 locals {
-  region = "us-east-1"
-  name   = "amonkincloud-cluster"
+  region = "ap-southeast-2"
+  name   = "mrkhalnayak_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-southeast-2a", "ap-southeast-2b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
   tags = {
     Example = local.name
   }
-}
-
-provider "aws" {
-  region = "us-east-1"
 }
